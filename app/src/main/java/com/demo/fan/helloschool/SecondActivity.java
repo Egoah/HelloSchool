0d73981d0a43f6518912aefcf3628c04cfd4e896
egoah <fjinhao@qq.com>
2016-07-02 07:56:29 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/fan/helloschool/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.demo.fan.helloschool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
